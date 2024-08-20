c03e3c8931378fe87fc94721d1d757e8a66ba5c3
Udomomo <udomomo@gmail.com>
2024-08-20 21:00:38 +0900

    change card style according to status

--- scrum-poker-client/src/app/card/card.component.ts
+++ scrum-poker-client/src/app/card/card.component.ts
@@ -1,15 +1,18 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './card.component.html',
   styleUrl: './card.component.css'
 })
 export class CardComponent {
   @Input() point: number = 0;
   @Input() name: string = "";
+  @Input() isSet: boolean = false;
+  @Input() isVisible: boolean = false;
   @Output() onClick = new EventEmitter<number>();
 
   handleClick() {
